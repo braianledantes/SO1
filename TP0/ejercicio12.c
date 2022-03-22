895846da764166752a2b404bd134bc2f8649e39f
Braian Ledantes <braianledantes@gmail.com>
2022-03-22 20:26:16 -0300

    listo ejercicio12

---
+++ TP0/ejercicio12.c
@@ -0,0 +1,125 @@
+/*
+12. Usando printf() y getchar(), desarrolle un video juego del ahorcado.
+    Utilice el código ahorcado.c como base.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>	/* para las funciones system y exit */
+
+char palabra [80];
+int tamanioPalabra = 0;
+
+char palabraAhorcado [80];
+int cantFallos = 0;
+
+int MAX_FALLOS = 6;
+
+int main() {
+    solicitarPalabra();
+    jugar();
+}
+
+void solicitarPalabra() {
+    char letra;
+
+    printf("Ingrese la palabra del ahorcado: \n");
+    
+    system ("/bin/stty raw");
+    
+    for(int i = 0; i < 79; i++) {
+        letra = getchar();
+        palabra[i] = letra;
+        
+        // si ingresa ENTER termina
+        if(letra == 13) {
+            tamanioPalabra = i;
+            palabra[i] = 0;
+            break;
+        }
+    }
+    printf("\r                                                                    ");
+    printf("\r                                                                    ");
+
+    system ("/bin/stty sane erase ^H");
+
+    // copia la palabra en el "pizarron"
+    for(int i = 0; i < tamanioPalabra; i++) {
+        if(i == tamanioPalabra)
+            palabraAhorcado[i] = 0;
+        else
+            palabraAhorcado[i] = '_';
+    }
+       
+}
+
+/**
+ * Verifica si la letra está en la palabra
+ * @return int 1 ó 0
+ */
+int letraAcertada(char c) {
+    int r = 0;
+    int i = 0;
+    while(r == 0 && i < tamanioPalabra) {
+        if (palabra[i] == c) {
+            r = 1;
+        } else {
+            i++;
+        }
+    }
+    return r;
+}
+
+void insertarLetra(char c) {
+    for(int i = 0; i < tamanioPalabra; i++) {
+        if (palabra[i] == c)
+        {
+            palabraAhorcado[i] = c;
+        }
+    }
+}
+
+int verificarPalabra() {
+    int finalizo = 1;
+    int i = 0;
+
+    do {
+        if(palabra[i] != palabraAhorcado[i]){
+            finalizo = 0;
+            break;
+        }
+        
+        i++;
+
+    } while(i < tamanioPalabra);
+
+    return finalizo;
+}
+
+void jugar() {
+    int c;
+
+	/* Decirle al sistema que el modo input es RAW */
+	system ("/bin/stty raw");
+
+    do {
+        printf("\r                                                                           ");
+		printf("\rPalabra: %s fallos: %i/%i  ingrese una letra (0 para salir): ", palabraAhorcado, cantFallos, MAX_FALLOS);
+        
+		c = getchar();
+
+        if (c == '0')
+            cantFallos = MAX_FALLOS;
+        
+        if (letraAcertada(c))
+            insertarLetra(c);
+        else 
+            cantFallos++;
+        
+    } while(verificarPalabra() == 0 && cantFallos < MAX_FALLOS);
+
+    printf("\r                                                                           ");
+	printf("\rPalabra: %s fallos: %i/%i  ingrese una letra (0 para salir): ", palabraAhorcado, cantFallos, MAX_FALLOS);
+
+	system ("/bin/stty sane erase ^H");
+    printf("\n");
+}
