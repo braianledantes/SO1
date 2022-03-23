085ab173077f2efc9333699ec713ec4c05862285
Braian Ledantes <braianledantes@gmail.com>
2022-03-23 19:46:21 -0300

    ejercicio12.c identado

--- TP0/ejercicio12.c
+++ TP0/ejercicio12.c
@@ -4,80 +4,93 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>	/* para las funciones system y exit */
+#include <stdlib.h> /* para las funciones system y exit */
 
 const int MAX_LENGHT = 80;
-char palabra [80];
+char palabra[80];
 int tamanioPalabra = 0;
 
-char palabraAhorcado [80];
+char palabraAhorcado[80];
 int cantFallos = 0;
 
 int MAX_FALLOS = 6;
 
-int main() {
+int main()
+{
     solicitarPalabra();
     jugar();
 }
 
-void solicitarPalabra() {
+void solicitarPalabra()
+{
     char c;
-    
-    system ("/bin/stty raw");
+
+    system("/bin/stty raw");
     int i = 0;
-    while(i < MAX_LENGHT - 1) {
+    while (i < MAX_LENGHT - 1)
+    {
         printf("\r                                                                           ");
         printf("\rIngrese la palabra del ahorcado: %s", palabra);
         c = getchar();
 
-        if((c >= 65 && c <= 90) || (c >= 97 && c <= 122)) { // si la letra es valida
+        if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+        { // si la letra es valida
             palabra[i] = c;
             i++;
         }
-        else if(c == 127) { // si se borro
+        else if (c == 127)
+        { // si se borro
             i--;
             if (i < 0)
                 i = 0;
             palabra[i] = 0;
         }
-        else if(c == 13) { // si se ingresa ENTER termina
+        else if (c == 13)
+        { // si se ingresa ENTER termina
             tamanioPalabra = i;
             palabra[i] = 0;
             break;
         }
     }
 
-    system ("/bin/stty sane erase ^H");
+    system("/bin/stty sane erase ^H");
 
     // copia la palabra en el "pizarron"
-    for(int i = 0; i < tamanioPalabra; i++) {
-        if(i == tamanioPalabra)
+    for (int i = 0; i < tamanioPalabra; i++)
+    {
+        if (i == tamanioPalabra)
             palabraAhorcado[i] = 0;
         else
             palabraAhorcado[i] = '_';
     }
-       
 }
 
 /**
  * Verifica si la letra está en la palabra
  * @return int 1 ó 0
  */
-int letraAcertada(char c) {
+int letraAcertada(char c)
+{
     int r = 0;
     int i = 0;
-    while(r == 0 && i < tamanioPalabra) {
-        if (palabra[i] == c) {
+    while (r == 0 && i < tamanioPalabra)
+    {
+        if (palabra[i] == c)
+        {
             r = 1;
-        } else {
+        }
+        else
+        {
             i++;
         }
     }
     return r;
 }
 
-void insertarLetra(char c) {
-    for(int i = 0; i < tamanioPalabra; i++) {
+void insertarLetra(char c)
+{
+    for (int i = 0; i < tamanioPalabra; i++)
+    {
         if (palabra[i] == c)
         {
             palabraAhorcado[i] = c;
@@ -85,49 +98,54 @@ void insertarLetra(char c) {
     }
 }
 
-int verificarPalabra() {
+int verificarPalabra()
+{
     int finalizo = 1;
     int i = 0;
 
-    do {
-        if(palabra[i] != palabraAhorcado[i]){
+    do
+    {
+        if (palabra[i] != palabraAhorcado[i])
+        {
             finalizo = 0;
             break;
         }
-        
+
         i++;
 
-    } while(i < tamanioPalabra);
+    } while (i < tamanioPalabra);
 
     return finalizo;
 }
 
-void jugar() {
+void jugar()
+{
     int c;
 
-	/* Decirle al sistema que el modo input es RAW */
-	system ("/bin/stty raw");
+    /* Decirle al sistema que el modo input es RAW */
+    system("/bin/stty raw");
 
-    do {
-        
-		printf("\rPalabra: %s fallos: %i/%i  ingrese una letra (0 para salir): ", palabraAhorcado, cantFallos, MAX_FALLOS);
-        
-		c = getchar();
+    do
+    {
+
+        printf("\rPalabra: %s fallos: %i/%i  ingrese una letra (0 para salir): ", palabraAhorcado, cantFallos, MAX_FALLOS);
+
+        c = getchar();
         if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
         {
             if (letraAcertada(c))
                 insertarLetra(c);
-            else 
+            else
                 cantFallos++;
         }
-        
+
         if (c == '0')
             cantFallos = MAX_FALLOS;
-        
-    } while(verificarPalabra() == 0 && cantFallos < MAX_FALLOS);
 
-	printf("\rPalabra: %s fallos: %i/%i  ingrese una letra (0 para salir): ", palabraAhorcado, cantFallos, MAX_FALLOS);
+    } while (verificarPalabra() == 0 && cantFallos < MAX_FALLOS);
 
-	system ("/bin/stty sane erase ^H");
+    printf("\rPalabra: %s fallos: %i/%i  ingrese una letra (0 para salir): ", palabraAhorcado, cantFallos, MAX_FALLOS);
+
+    system("/bin/stty sane erase ^H");
     printf("\n");
 }
