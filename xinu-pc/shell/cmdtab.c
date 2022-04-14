78a48bf1d1f63d8bbea4b94e5571977d87f7423f
Braian Ledantes <braianledantes@gmail.com>
2022-04-14 20:38:21 -0300

    de vuelta desde cero

--- xinu-pc/shell/cmdtab.c
+++ xinu-pc/shell/cmdtab.c
@@ -27,11 +27,8 @@ const	struct	cmdent	cmdtab[] = {
 	{"udpecho",	FALSE,	xsh_udpecho},
 	{"udpeserver",	FALSE,	xsh_udpeserver},
 	{"uptime",	FALSE,	xsh_uptime},
-	{"?",		FALSE,	xsh_help},
-	{"programa",    FALSE,  mi_programa},
-	{"ejer5", FALSE, ejer5},
-	{"ejer6", FALSE, ejer6},
-	{"ahorcado", FALSE, ahorcado}
+	{"?",		FALSE,	xsh_help}
+
 };
 
 uint32  ncmd = sizeof(cmdtab) / sizeof(struct cmdent);
