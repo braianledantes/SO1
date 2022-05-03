ce9dcdcff28d093d30f676cfa923d2649b3d578d
Braian Ledantes <braianledantes@gmail.com>
2022-05-03 18:40:08 -0300

    ahorcado corregido

--- xinu-pc/shell/ahorcadoparcial.c
+++ xinu-pc/shell/ahorcadoparcial.c
@@ -8,7 +8,7 @@ int vidas = 5;
 int restante = 8;
 char c;
 
-sid32 mutex, entrada, computar;
+sid32 mutex, entrada, computar, terminar;
 
 void p1(void), p2(void), p3(void), p4(void);
 pid32 pid_p1, pid_p2, pid_p3, pid_p4;
@@ -18,6 +18,7 @@ void ahorcadoparcial()
 	entrada = semcreate(1);
 	computar = semcreate(0);
 	mutex = semcreate(1);
+	terminar = semcreate(0);
 
 	pid_p1 = create(p1, 1024, 20, "p1", 0);
 	pid_p2 = create(p2, 1024, 20, "p2", 0);
@@ -31,6 +32,10 @@ void ahorcadoparcial()
 	resume(pid_p2);
 	resume(pid_p3);
 	resume(pid_p4);
+
+	wait(terminar);
+
+	control(0, TC_MODEC, 0, 0);
 }
 
 void p1()
@@ -108,4 +113,6 @@ void p4()
 	kill(pid_p1);
 	kill(pid_p2);
 	kill(pid_p3);
+
+	signal(terminar);
 }
